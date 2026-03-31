bbaebe656426eafebb4e92f4e92e22fc8fb1199e
celova <nfxacademymd@gmail.com>
2026-03-31 10:44:19 +0100

    Update nba_fetcher.py

--- app/data/nba_fetcher.py
+++ app/data/nba_fetcher.py
@@ -1,1 +1,15 @@
+from nba_api.live.endpoints import scoreboard
+from nba_api.stats.static import teams
+import json
 
+def get_todays_games():
+    board = scoreboard.ScoreBoard()
+    games = board.get_dict()["scoreboard"]["games"]
+    return [
+        {
+            "game_id": g["gameId"],
+            "home": g["homeTeam"]["teamTricode"],
+            "away": g["awayTeam"]["teamTricode"],
+            "status": g["gameStatusText"]
+        } for g in games
+    ]
